6662179396f62425ec542ebcef969b5b17a6efa6
Wikel <riley.wikel@cognizant.com>
2023-11-16 17:03:08 -0600

    Changed the page review component to async call reviews and also changed dk game image

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,41 +1,33 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getReviews } from "@/lib/reviews";
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-5">
-        <li className="max-w-sm bg-background-white-light rounded-lg shadow hover:shadow-xl">
-          <Link
-            href="/reviews/smash-bros-ultimate"
-            className="text-font-color font-orbitron hover:underline"
+      <ul className="flex flex-row flex-wrap gap-5">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="max-w-sm bg-background-white-light rounded-lg shadow hover:shadow-xl"
           >
-            <img
-              src="/images/smash.jpg"
-              className="mb-3 rounded-t-lg"
-              alt="Super Smash Bros. Ultimate for the Nintendo Switch"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Super Smash Bros. Ultimate
-            </h2>
-          </Link>
-        </li>
-        <li className="max-w-sm bg-background-white-light rounded-lg shadow hover:shadow-xl">
-          <Link
-            href="/reviews/super-mario-odyssey"
-            className="text-font-color hover:underline"
-          >
-            <img
-              src="/images/mario odyssey.jpg"
-              className="mb-3 rounded-t-lg"
-              alt="Super Smash Bros. Ultimate for the Nintendo Switch"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Super Mario Odyssey
-            </h2>
-          </Link>
-        </li>
+            <Link
+              href={`/reviews/${review.slug}`}
+              className="text-font-color font-orbitron hover:underline"
+            >
+              <img
+                src={review.image}
+                className="mb-3 rounded-t-lg"
+                alt={`${review.title} for the Nintendo Switch`}
+              />
+              <h2 className="py-1 text-center font-orbitron font-semibold">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
---
+++ content/reviews/donkey-kong-64.md
@@ -0,0 +1,9 @@
+---
+title: "Donkey Kong 64"
+date: "2023-11-14"
+image: "/images/donkey kong 64.jpg"
+---
+
+__Donkey Kong 64__ is a 1999 platform game developed by Rare and published by Nintendo for the Nintendo 64. It is the only Donkey Kong game to feature 3D gameplay. As the gorilla Donkey Kong, the player explores themed levels to collect items and rescue his kidnapped family members from King K. Rool. The player completes minigames and puzzles as five playable Kong characters—each with their own special abilities—to receive bananas and other collectibles. In multiplayer modes, up to four players can compete in deathmatch and last man standing games.
+
+Rare began working on Donkey Kong 64 in 1997, following the completion of Donkey Kong Country 3 (1996). It was conceived as a 2.5D platformer similar to Country before becoming a more open-ended game using the engine from Rare's Banjo-Kazooie (1998). A 16-person team with many recruits from the Banjo group finished it in 1999. It was published by Nintendo in North America in November and worldwide in December. Donkey Kong 64 was the first game to require the Nintendo 64 Expansion Pak, an accessory that added memory resources. The US$22 million marketing campaign included advertisements, sweepstakes, and a national tour. 
